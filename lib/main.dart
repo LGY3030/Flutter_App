c4843ff5d6b674b6d7206cdb368305324792ec8c
LGY3030 <lgy3030@gmail.com>
2018-05-07 03:43:12 +0800

    Update
    Add animation of the logo and textfield of login page

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,21 @@ class FlutterApp extends StatelessWidget {
        );
 }
 }
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatefulWidget{
   @override
   State createState() => new LoginPageState();
 }
-class LoginPageState extends State<LoginPage>{
+class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController=new AnimationController(vsync: this,duration: new Duration(milliseconds: 500));
+    _iconAnimation=CurvedAnimation(parent: _iconAnimationController, curve: Curves.bounceOut);
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
   @override
   Widget build(BuildContext context){
     return new Scaffold(
@@ -33,13 +43,30 @@ class LoginPageState extends State<LoginPage>{
               color: Colors.black54
             ),
           new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               new Image(
-
                   image: new AssetImage("images/Logo.png"),
-                  width: 200.0,
-              )
+                  width: _iconAnimation.value*140.0,
+              ),
+              new Form(
+               // autovalidate: true,
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "email"
+                      ),
+                    ),
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "password"
+                      ),
+                    )
+                  ],
+                ),
+                  )
             ],
           )
         ],
