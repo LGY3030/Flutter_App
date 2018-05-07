a3a1daf1d9f1dfd3c9f98c65d6493554fe88eb70
LGY3030 <lgy3030@gmail.com>
2018-05-08 02:56:58 +0800

    Update
    Add a Login button, change some themedata, and finish a simple login page

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixi
   @override
   void initState() {
     super.initState();
-    _iconAnimationController=new AnimationController(vsync: this,duration: new Duration(milliseconds: 500));
+    _iconAnimationController=new AnimationController(vsync: this,duration: new Duration(milliseconds: 800));
     _iconAnimation=CurvedAnimation(parent: _iconAnimationController, curve: Curves.bounceOut);
     _iconAnimation.addListener(() => this.setState(() {}));
     _iconAnimationController.forward();
@@ -42,33 +42,63 @@ class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixi
               colorBlendMode: BlendMode.darken,
               color: Colors.black54
             ),
-          new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              new Image(
-                  image: new AssetImage("images/Logo.png"),
-                  width: _iconAnimation.value*140.0,
-              ),
-              new Form(
-               // autovalidate: true,
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new TextFormField(
-                      decoration: new InputDecoration(
-                        labelText: "email"
-                      ),
-                    ),
-                    new TextFormField(
-                      decoration: new InputDecoration(
-                        labelText: "password"
-                      ),
-                    )
-                  ],
+          new Theme(
+            data: new ThemeData(
+              brightness: Brightness.dark,
+              inputDecorationTheme:new InputDecorationTheme(
+                labelStyle: new TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 20.0
+                )
+              )
+            ),
+            isMaterialAppTheme: true,
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new Image(
+                    image: new AssetImage("images/Logo.png"),
+                    width: _iconAnimation.value*140.0,
                 ),
-                  )
-            ],
-          )
+                new Container(
+                  padding: const EdgeInsets.all(40.0),
+                  child: new Form(
+                    autovalidate: true,
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "email"
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "password"
+                          ),
+                          obscureText: true,
+                          keyboardType: TextInputType.text,
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 60.0),
+                        ),
+                        new MaterialButton(
+                          height: 40.0,
+                          minWidth: 80.0,
+                          color: Colors.green,
+                          splashColor: Colors.orangeAccent,
+                          textColor: Colors.black,
+                          child: new Text("Log in"),
+                          onPressed: () => {},
+                        )
+                      ],
+                    ),
+                      ),
+                )
+              ],
+            ),
+          ),
         ],
       )
     );
